b902fcb7761f0d3a39ffe2d241169af4c8f3a3ff
Ramon Moñino <ramonmr16@gmail.com>
2020-04-02 20:47:12 +0200

    Added CarsComponent selector to app component. Added CarsComponent declaration, HttpClientModule import and CarService as a provider to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CarsComponent } from "./components/cars/cars.component";
+
+import { CarService } from "./services/car.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, CarsComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

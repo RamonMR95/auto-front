99d7bbe1f1584bef7d48beffe206e252a7346927
Ramon Moñino <ramonmr16@gmail.com>
2020-04-14 20:00:09 +0200

    Added routes to edit and create a car.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 
 import { CarsComponent } from "./components/cars/cars.component";
-import { CarDetailComponent } from './components/car-detail/car-detail.component';
+import { CarDetailComponent } from "./components/car-detail/car-detail.component";
+import { CarFormComponent } from "./components/car-form/car-form.component";
 
 export const appRoutes: Routes = [
   { path: "cars", component: CarsComponent },
-  { path: "car/:id", component: CarDetailComponent }
+  { path: "car/:id", component: CarDetailComponent },
+  { path: "create", component: CarFormComponent },
+  { path: "car/edit/:id", component: CarFormComponent }
 ];
 
 @NgModule({

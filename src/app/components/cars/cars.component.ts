2ae507658d9fae8ba6f56fe53323551ec8b92768
Ramon Moñino <ramonmr16@gmail.com>
2020-04-14 19:59:15 +0200

    Added angular material to cars component and navbar selector to app component.

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 import { CarService } from "../../services/car.service";
 
 import { Car } from "src/app/models/car.model";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
 
 @Component({
   selector: "app-cars",
   templateUrl: "./cars.component.html",
-  styleUrls: ["./cars.component.css"]
+  styleUrls: ["./cars.component.css"],
 })
 export class CarsComponent implements OnInit {
+  displayedColumns: string[] = ["id", "brand", "registration", "country"];
+  dataSource: MatTableDataSource<Car>;
   cars: Car[];
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private carService: CarService) {}
 
@@ -19,6 +27,20 @@ export class CarsComponent implements OnInit {
   }
 
   getCars(): void {
-    this.carService.getCars().then(res => (this.cars = res));
+    this.carService.getCars().then((res) => {
+      this.cars = res;
+      this.dataSource = new MatTableDataSource(this.cars);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }

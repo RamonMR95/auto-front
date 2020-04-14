b34c319b7750dd110b8a21be4f1627b0f2f902cb
Ramon Moñino <ramonmr16@gmail.com>
2020-04-14 19:50:45 +0200

    Added http methods to create, update and delete a car, material module and navbar component.

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 import { Car } from "../models/car.model";
 
 import { API_URL } from "../config/config";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
 
 @Injectable({
   providedIn: "root"
@@ -18,5 +24,21 @@ export class CarService {
 
   getCar(id: string): Promise<Car> {
     return this.httpClient.get<Car>(`${API_URL}/${id}`).toPromise();
+  }
+
+  createCar(car: Car): Promise<Car> {
+    return this.httpClient.post<Car>(API_URL, car, httpOptions).toPromise();
+  }
+
+  updateCar(id: string, car: Car): Promise<Car> {
+    return this.httpClient
+      .put<Car>(`${API_URL}/${id}`, car, httpOptions)
+      .toPromise();
+  }
+
+  deleteCar(id: string): Promise<Car> {
+    return this.httpClient
+      .delete<Car>(`${API_URL}/${id}`, httpOptions)
+      .toPromise();
   }
 }

060d243182885b6ca7f920eb8fe1d5523109927b
Ramon Moñino <ramonmr16@gmail.com>
2020-04-23 16:49:11 +0200

    Added backend pagination, ordering, filtering to cars component.

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -4,7 +4,7 @@ import { CarService } from "../../services/car.service";
 
 import { Car } from "src/app/models/car.model";
 import { MatTableDataSource } from "@angular/material/table";
-import { MatPaginator } from "@angular/material/paginator";
+import { MatPaginator, PageEvent } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 
 @Component({
@@ -16,31 +16,64 @@ export class CarsComponent implements OnInit {
   displayedColumns: string[] = ["id", "brand", "registration", "country"];
   dataSource: MatTableDataSource<Car>;
   cars: Car[];
+  page: number = 1;
+  pageSize: number;
+  length: number;
+  orderBy: string;
+  filter: string;
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  constructor(private carService: CarService) {}
+  constructor(private carService: CarService) { }
 
   ngOnInit(): void {
+    this.pageSize = this.paginator.pageSize || 5;
     this.getCars();
   }
 
   getCars(): void {
-    this.carService.getCars().then((res) => {
-      this.cars = res;
+    this.page = this.paginator.pageIndex + 1;
+    this.carService.getCars(this.page, this.pageSize).then((res) => {
+      this.cars = res.cars;
+      this.length = res.total_count;
       this.dataSource = new MatTableDataSource(this.cars);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
     });
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  applyBackendFilter(event: Event) {
+    this.page = 1;
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+
+    this.carService
+      .getCars(this.page, this.pageSize, this.filter)
+      .then((res) => {
+        this.cars = res.cars;
+        this.length = res.total_count;
+        this.dataSource.data = this.cars;
+      });
+  }
+
+  setOffset(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.page = event.pageSize * event.pageIndex;
+    this.getCars();
+  }
+
+  sortChange(event): void {
+    let field = event.active;
+    let direction = event.direction;
+    if (direction === "desc") {
+      this.orderBy = "-" + field;
+    }
+
+    this.carService.getCars(this.page, this.pageSize, this.filter, this.orderBy).then((res) => {
+      this.cars = res.cars;
+      this.length = res.total_count;
+      this.dataSource.data = this.cars;
+    });
   }
 }
